5ee6e19b5071bb653e02dfc3a13c39007ec8a32c
Takehiko Konuma <TakehikoKonuma@Takehiko-no-MacBook-Pro.local>
2018-01-20 17:56:34 +0900

    edit everyhing especially in facebook omni

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+
          has_many :listings
 
     def self.from_omniauth(auth)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root :to => 'pages#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',:registrations => 'registrations' }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
 
   resources :users, only: [:show]
 
